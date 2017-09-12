72b66e390fc734210717232b9807437afb0109b9
wangchifu <wangchifu@gmail.com>
2017-09-12 13:30:51 +0800

    fix content

--- resources/views/admin/contentAdminEdit.blade.php
+++ resources/views/admin/contentAdminEdit.blade.php
@@ -36,7 +36,8 @@
                         filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
                         filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
                         filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
-                        filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+                        filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token=',
+                        allowedContent: true,
                     };
                 </script>
                 <script>
